23d4065901aae272a983791e369dcc3441db586a
SuperH <SuperHuJ@163.com>
2018-06-21 08:15:11 +0800

    项目注册之图片验证码

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+
+# url('^image_codes/(?P<image_code_id>[\w-]+)/$', views.ImageCodeView.as_view()),
+class ImageCodeView(APIView):
+    """
+    图片验证码
+    """
