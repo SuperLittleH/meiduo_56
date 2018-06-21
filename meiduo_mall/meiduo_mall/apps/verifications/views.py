5e95728eae8835b48a498c2a6e8cd3731f03a7f8
SuperH <SuperHuJ@163.com>
2018-06-21 09:17:01 +0800

    图片验证码后端实现

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django_redis import get_redis_connection
 from rest_framework.views import APIView
+from django.http import HttpResponse
+
+from meiduo_mall.libs.captcha.captcha import captcha
+from verifications import constants
 
 # Create your views here.
 
@@ -8,3 +13,17 @@ class ImageCodeView(APIView):
     """
     图片验证码
     """
+    def get(self,request,image_code_id):
+        """
+        获取短信验证码
+        :param request:
+        :param image_code_id:
+        :return:
+        """
+        #生成图片验证码
+        text,image = captcha.generate_captcha()
+
+        redis_conn = get_redis_connection('verify_codes')
+        redis_conn.setex("img_%s" % image_code_id,constants.IMAGE_CODE_REDIS_EXPIRES,text)
+        # 固定返回验证码的图片数据不需要rest
+        return HttpResponse(image,content_type="image/jpg")
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -105,6 +105,13 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
+    },
+    "verify_codes": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
