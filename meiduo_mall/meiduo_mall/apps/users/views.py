721b750dcddd82d1b65378689fbe9176ce6a39c9
SuperH <SuperHuJ@163.com>
2018-06-22 12:11:42 +0800

    注册实现

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,4 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from users.models import User
+from . import serializers
 # Create your views here.
 
+
+class UsernameCountView(APIView):
+    """
+      用户名数量
+    """
+    def get(self,request,username):
+        """
+        获取用户名数量
+        :param request:
+        :param username:
+        :return:
+        """
+        count = User.objects.filter(username=username).count()
+        data = {
+            'username':username,
+            'count':count
+        }
+
+        return Response(data)
+
+
+class MobileCountView(APIView):
+    """
+    手机号数量
+    """
+    def get(self, request, mobile):
+        """
+        获取指定手机号数量
+        """
+        count = User.objects.filter(mobile=mobile).count()
+
+        data = {
+            'mobile': mobile,
+            'count': count
+        }
+
+        return Response(data)
+
+
+class UserView(CreateAPIView):
+    """
+    用户注册
+    """
+    serializer_class = serializers.CreateUserSerializer
