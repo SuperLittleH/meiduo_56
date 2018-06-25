a806fb336a637aa7f40c79b03d282e9d4dbb4797
SuperH <SuperHuJ@163.com>
2018-06-25 18:14:12 +0800

    路由添加

---
+++ meiduo_mall/meiduo_mall/apps/areas/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+urlpatterns = [
+
+]
+
+router = DefaultRouter()
+router.register(r'areas',views.AreaViewSet,base_name='areas')
+
+urlpatterns += router.urls
+
+
