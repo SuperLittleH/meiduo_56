39f8e0dc9bdbeb1af1959a153653f32c2bf98a57
SuperH <SuperHuJ@163.com>
2018-06-29 00:39:22 +0800

    商品详情静态化测试

--- meiduo_mall/scripts/regenerate_detail_html.py
+++ meiduo_mall/scripts/regenerate_detail_html.py
@@ -1,13 +1,28 @@
-from celery_tasks.main import celery_app
+#!/usr/bin/env python
+
+"""
+功能：手动生成所有SKU的静态detail html文件
+使用方法:
+    ./regenerate_detail_html.py
+"""
+import sys
+sys.path.insert(0, '../')
+sys.path.insert(0, '../meiduo_mall/apps')
+
+import os
+if not os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'meiduo_mall.settings.dev'
+
+import django
+django.setup()
+
 from django.template import loader
 from django.conf import settings
-import os
 
 from goods.utils import get_categories
 from goods.models import SKU
 
 
-@celery_app.task(name='generate_static_sku_detail_html')
 def generate_static_sku_detail_html(sku_id):
     """
     生成静态商品详情页面
@@ -25,7 +40,6 @@ def generate_static_sku_detail_html(sku_id):
     goods.channel = goods.category1.goodschannel_set.all()[0]
 
     # 构建当前商品的规格键
-    # sku_key = [规格1参数id， 规格2参数id， 规格3参数id, ...]
     sku_specs = sku.skuspecification_set.order_by('spec_id')
     sku_key = []
     for spec in sku_specs:
@@ -52,23 +66,6 @@ def generate_static_sku_detail_html(sku_id):
         spec_sku_map[tuple(key)] = s.id
 
     # 获取当前商品的规格信息
-    #specs = [
-    #    {
-    #        'name': '屏幕尺寸',
-    #        'options': [
-    #            {'value': '13.3寸', 'sku_id': xxx},
-    #            {'value': '15.4寸', 'sku_id': xxx},
-    #        ]
-    #    },
-    #    {
-    #        'name': '颜色',
-    #        'options': [
-    #            {'value': '银色', 'sku_id': xxx},
-    #            {'value': '黑色', 'sku_id': xxx}
-    #        ]
-    #    },
-    #    ...
-    #]
     specs = goods.goodsspecification_set.order_by('id')
     # 若当前sku的规格信息不完整，则不再继续
     if len(sku_key) < len(specs):
@@ -98,3 +95,10 @@ def generate_static_sku_detail_html(sku_id):
     file_path = os.path.join(settings.GENERATED_STATIC_HTML_FILES_DIR, 'goods/'+str(sku_id)+'.html')
     with open(file_path, 'w') as f:
         f.write(html_text)
+
+
+if __name__ == '__main__':
+    skus = SKU.objects.all()
+    for sku in skus:
+        print(sku.id)
+        generate_static_sku_detail_html(sku.id)
