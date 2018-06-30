ba89d3c3056374067992e31d426319c4f5bba868
SuperH <SuperHuJ@163.com>
2018-06-30 10:31:08 +0800

    搜索后端实现

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
@@ -7,3 +8,6 @@ urlpatterns = [
    url(r'^categories/(?P<category_id>\d+)/skus/$',views.SKUListView.as_view()),
 ]
 
+router = DefaultRouter()
+router.register('skus/search', views.SKUSearchViewSet,base_name='skus_search')
+urlpatterns += router.urls
--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
+from drf_haystack.viewsets import HaystackViewSet
 
 from goods.models import SKU
-from users import serializers
+from users.serializers import SKUSerializer
+from . import serializers
 
 
 class SKUListView(ListAPIView):
@@ -11,11 +13,21 @@ class SKUListView(ListAPIView):
     sku列表数据
     """
 
-    serializer_class = serializers.SKUSerializer
+    serializer_class = SKUSerializer
     filter_backends = (OrderingFilter,)
     ordering_fields = ('create_time,price,sales')
 
     def get_queryset(self):
         category_id = self.kwargs['category_id']
         return SKU.objects.filter(category_id=category_id,is_launched=True)
+
+
+class SKUSearchViewSet(HaystackViewSet):
+    """
+    SKU搜索
+    """
+    index_models = [SKU]
+
+    serializer_class = serializers.SKUIndexSerializer
+
 
