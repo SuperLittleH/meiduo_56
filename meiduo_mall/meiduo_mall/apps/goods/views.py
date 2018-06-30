89f0207dea885db85b3aed23bdba3f6d59db0e1b
SuperH <SuperHuJ@163.com>
2018-06-30 09:00:12 +0800

    商品列表页后端实现

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -10,10 +10,11 @@ class SKUListView(ListAPIView):
     """
     sku列表数据
     """
+
     serializer_class = serializers.SKUSerializer
     filter_backends = (OrderingFilter)
     ordering_fields = ('create_time,price,sales')
-    
+
     def get_queryset(self):
         category_id = self.kwargs['category_id']
         return SKU.objects.filter(category_id=category_id,is_launched=True)
