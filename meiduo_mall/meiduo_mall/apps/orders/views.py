4657afe0af5d971825e9655378679c85a59ac19e
SuperH <SuperHuJ@163.com>
2018-07-03 18:29:03 +0800

    订单确认页面实现

--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -1,3 +1,46 @@
+from decimal import Decimal
 from django.shortcuts import render
+from django_redis import get_redis_connection
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from goods.models import SKU
+from orders import serializers
+
+
+class OrderSettlementView(APIView):
+    """
+    订单结算
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        """
+        获取
+        :param request:
+        :return:
+        """
+        user = request.user
+
+
+        # 从购物车中获取用户勾选要结算的商品信息
+        redis_conn = get_redis_connection('cart')
+        redis_cart = redis_conn.hgetall('cart_%s' % user.id)
+        cart_selected = redis_conn.smembers('cart_selected_%s' % user.id)
+
+        cart = {}
+
+        for sku_id in cart_selected:
+            cart[int(sku_id)] = int(redis_cart[sku_id])
+
+        # 查询商品信息
+        skus = SKU.objects.filter(id__in=cart.keys())
+        for sku in skus:
+            sku.count = cart[sku.id]
+
+        #运费
+        freight = Decimal('10.00')
+
+        serializer = serializers.OrderSettlementSerializer({'freight':freight,'skus':skus})
+        return Response(serializer.data)
