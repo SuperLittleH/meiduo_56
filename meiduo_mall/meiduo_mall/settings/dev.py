35b3e22a8ed37dee50fd84fe3a4e90a6408ab026
SuperH <SuperHuJ@163.com>
2018-07-04 23:11:55 +0800

    支付宝发起支付后端实现

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -327,3 +327,9 @@ HAYSTACK_CONNECTIONS = {
 # 当添加、修改、删除数据时，自动生成索引
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
+# 支付宝
+ALIPAY_APPID = "2016091300505049"
+ALIPAY_URL = "https://openapi.alipaydev.com/gateway.do"
+ALIPAY_DEBUG = True
+
+
